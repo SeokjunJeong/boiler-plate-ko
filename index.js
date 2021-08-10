83e419c668c288936886d1dcfaaf3172ff24f9a4
SeokjunJeong <anjal0322@gmail.com>
2021-08-10 22:01:01 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://jun:qwer1234!@boilerplate.hlwkc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("mongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!~~안녕하세요"));
+
+app.listen(port, () => console.log("Example app listening on port ${port}!"));
